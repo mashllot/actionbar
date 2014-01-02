7b3f72b483f00ea49b0fd4d4350d918399a7a92f
Zak Chapman <newtechjp@live.com>
2014-01-03 02:58:51 +0900

    ActionBar used for AIDLServicesSensors

---
+++ gen/com/zak/aidlsensorsservices/widget/actionbar/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.zak.aidlsensorsservices.widget.actionbar;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
